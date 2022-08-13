173483a1b72be5f5dadb8c4d1fa7d33ea8711a66
GuentherAtThePhone <71941790+GuentherAtThePhone@users.noreply.github.com>
2022-08-13 15:07:43 +0200

    Updated Settings files

--- HueControl/Properties/Settings.Designer.cs
+++ HueControl/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace HueControl.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.2.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.3.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -25,7 +25,7 @@ namespace HueControl.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        [global::System.Configuration.DefaultSettingValueAttribute("192.168.178.47")]
         public string BridgeIP {
             get {
                 return ((string)(this["BridgeIP"]));
@@ -37,7 +37,7 @@ namespace HueControl.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        [global::System.Configuration.DefaultSettingValueAttribute("bU14XCakBfOQWw92eqGOg9CeJuTqAcPJMafh0xLd")]
         public string Usercode {
             get {
                 return ((string)(this["Usercode"]));
